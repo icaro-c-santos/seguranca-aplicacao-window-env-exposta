18a42a4fd7c1ebfae713e6bc49007dc317401413
Icaro C. Santos <82792724+icaro-c-santos@users.noreply.github.com>
2025-06-19 10:41:54 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{ padding: 20 }}>
+    <div style={{ padding: 25 }}>
       <h1>DevSecOps Demo</h1>
       <p>API Key: {window.env.API_KEY}</p>
       <p>Payment URL: {window.env.PAY_URL}</p>
